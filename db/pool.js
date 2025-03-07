985b946b50dd3ce025c9c8c7c3dfc22f0edb4ea9
RhexNiebres <rhexniebres@gmail.com>
2025-03-06 16:24:09 -0800

    enable environment variables using dotenv

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
+require("dotenv").config(); 
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
     host: process.env.DB_HOST || "127.0.0.1",
     user: process.env.DB_USER,
